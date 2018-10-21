887bc9abb2a9509a247846089aeb1073ee7ef463
Joshua <44331460+hdc-joshua@users.noreply.github.com>
2018-10-21 12:38:38 +0800

    Update tawkto.php

--- tawkto.php
+++ tawkto.php
@@ -26,7 +26,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/5aeda9b25f7cdf4f0533e3d1/default';
+s1.src='https://embed.tawk.to/{$siteId}/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
