960083566c0cb939af5bd7982dcac8e660b2af24
Joshua <44331460+hdc-joshua@users.noreply.github.com>
2018-10-26 17:57:31 +0800

    Update tawkto.php

--- tawkto.php
+++ tawkto.php
@@ -3,16 +3,12 @@ namespace Grav\Plugin;
 use Grav\Common\Plugin;
 class tawktoPlugin extends Plugin
 {
-    /**
-     * @return array
-     */
     public static function getSubscribedEvents()
     {
         return [
             'onAssetsInitialized' => ['onAssetsInitialized', 0]
         ];
     }
-   
     public function onAssetsInitialized()
     {
         if ($this->isAdmin()) {
@@ -20,10 +16,7 @@ class tawktoPlugin extends Plugin
         }
         $siteId = trim($this->config->get('plugins.tawkto.siteId'));
         if ($siteId) {
-            $init = "
-//<!--Start of Tawk.to Script-->
-
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            $init = "var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement(\"script\"),s0=document.getElementsByTagName(\"script\")[0];
 s1.async=true;
@@ -31,13 +24,7 @@ s1.src='https://embed.tawk.to/{$siteId}/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
-<script type=\"text/javascript\">
-
-
-            ";
+})();";
             $this->grav['assets']->addInlineJs($init);
         }
     }
